012e04390e8a1631279f63fdf861a76330a32b01
darleneDiPierro <rosequinzley@gmail.com>
2022-07-16 12:04:44 +0700

    commit to main

--- process/daftar_pengajuan/uploadfile.php
+++ process/daftar_pengajuan/uploadfile.php
@@ -2,18 +2,32 @@
 require_once('../../function/helper.php');
 require_once('../../function/koneksi.php');
 
-// include("../../aset/data/lol");
 
 $nim = $_POST['nim'];
 $nama = $_POST['nama'];
-$persyaratan = $_FILES['persyaratan']['name'];
-// $persetujuan = $_POST['persetujuan'];
-// $kbfile = $_POST['kbfile'];
-// $kbimg = $_POST['kbimg'];
 
-$tmpFile = $_FILES['persyaratan']['tmp_name'];
+$tmpFilePersyaratan = $_FILES['persyaratan']['tmp_name'];
+$namePersyaratan = $_FILES['persyaratan']['name'];
 
-move_uploaded_file($tmpFile,"../../aset/data/".$persyaratan);
+$tmpFilePersetujuan = $_FILES['persetujuan']['tmp_name'];
+$namePersetujuan = $_FILES['persetujuan']['name'];
 
+$tmpKbFile = $_FILES['kbfile']['tmp_name'];
+$nameKbFile = $_FILES['kbfile']['name'];
+
+$tmpKbImg = $_FILES['kbimg']['tmp_name'];
+$nameKbImg = $_FILES['kbimg']['name'];
+
+$dir = "../../aset/data/";
+
+move_uploaded_file($tmpFilePersyaratan,$dir.$namePersyaratan);
+
+if(empty($sesi) || empty($namamhs) || empty($namadosen1) || empty($namadosen2) || empty($tgl)){
+    header("location: ".BASE_URL.'dashboard.php?page=daftarSidang&process=failed');
+} else {
+    mysqli_query($koneksi, "INSERT INTO jadwalsidang(sesi,namamhs,namadosen1,namadosen2,tgl) VALUES ('$sesi','$namamhs','$namadosen1','$namadosen2','$tgl')");
+
+    header("location: ".BASE_URL.'dashboard.php?page=datajadwalsidang&process=success');
+}
 
 ?>
